0cb8f6e1e2f3e5ae71dd9be1e45c71c37a4d1a4f
Marcos Vinicius <marcos.rso@live.com>
2021-04-21 12:11:20 -0300

    Hello world Material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, Typography, Grid } from '@material-ui/core';
+import { Public as PublicIcon } from '@material-ui/icons';
+import muiTheme from './styles/light';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Grid container justify="center" spacing={2}>
+        <Grid item>
+          <PublicIcon color="primary" />
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" color="primary" align="center">
+            Hello world
+          </Typography>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
